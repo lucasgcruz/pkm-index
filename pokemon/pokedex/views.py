aff12fe5202f15d69c645639ae23025f47372097
Lucas Gomes Cruz <100975198+lucasgcruz@users.noreply.github.com>
2022-08-30 20:50:50 +0000

    otimização de codigo

--- pokemon/pokedex/views.py
+++ pokemon/pokedex/views.py
@@ -18,48 +18,17 @@ def index(request):
     peso = pokemon['weight']
     altura = pokemon['height']
 
-    if peso < 10:
-      pass
-    
-    elif peso < 100:
-        pesolista = list(str(peso))
-        pesolista.insert(1, '.')
-        peso = ''.join(pesolista)
-      
-    elif peso < 1000:
-        pesolista = list(str(peso))
-        pesolista.insert(2, '.')
-        peso = ''.join(pesolista)
-      
-    elif peso < 10000:
-        pesolista = list(str(peso))
-        pesolista.insert(3, '.')
-        peso = ''.join(pesolista)
-    else:
-        peso = 'NaN'
+    if peso<10:0
+    elif peso<100:pesolista=list(str(peso));pesolista.insert(1,'.');peso=''.join(pesolista)
+    elif peso<1000:pesolista=list(str(peso));pesolista.insert(2,'.');peso=''.join(pesolista)
+    elif peso<10000:pesolista=list(str(peso));pesolista.insert(3,'.');peso=''.join(pesolista)
+    else:peso='NaN'
 
-    if altura < 10:
-      alturalista = list(str(altura))
-      alturalista.insert(0, '0.')
-      altura = ''.join(alturalista)
-      
-    elif altura < 100:
-      alturalista = list(str(altura))
-      alturalista.insert(1, '.')
-      altura = ''.join(alturalista)
-  
-    elif altura < 1000:
-      alturalista = list(str(altura))
-      alturalista.insert(2, '.')
-      altura = ''.join(alturalista)
-    
-    elif altura < 10000:
-      alturalista = list(str(altura))
-      alturalista.insert(3, '.')
-      altura = ''.join(alturalista)
-
-    else:
-      altura = 'NaN'
+    if altura<10:alturalista=list(str(altura));alturalista.insert(0,'0.');altura=''.join(alturalista)
+    elif altura<100:alturalista=list(str(altura));alturalista.insert(1,'.');altura=''.join(alturalista)
+    elif altura<1000:alturalista=list(str(altura));alturalista.insert(2,'.');altura=''.join(alturalista)
+    elif altura<10000:alturalista=list(str(altura));alturalista.insert(3,'.');altura=''.join(alturalista)
+    else:altura='NaN'
 
     return render(request, 'index.html',{
     'fotopokemon': pokemon['sprites']['other']['official-artwork']['front_default'],
