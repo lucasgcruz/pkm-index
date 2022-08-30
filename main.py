fef3e81f3acc853656094790a53bbfd946d9b4ca
Lucas Gomes Cruz <100975198+lucasgcruz@users.noreply.github.com>
2022-08-30 20:46:13 +0000

    implementação de peso e altura

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-dados_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon/weezing').json()
+dados_pokemon = requests.get('https://pokeapi.co/api/v2/pokemon/pidgey').json()
 
 nome = dados_pokemon['name'].upper()
 id = dados_pokemon['id']
@@ -10,4 +10,47 @@ peso = dados_pokemon['weight']
 altura = dados_pokemon['height']
 tipo1pokemon = dados_pokemon['types'][0]['type']['name']
 tipo2pokemon = dados_pokemon['types'][0]['type']['name']
-print(tipo1pokemon)
+
+print(peso)
+
+if peso < 10:
+  pass
+
+elif peso < 100:
+    pesolista = list(str(peso))
+    pesolista.insert(1, '.')
+    peso = ''.join(pesolista)
+  
+elif peso < 1000:
+    pesolista = list(str(peso))
+    pesolista.insert(2, '.')
+    peso = ''.join(pesolista)
+  
+elif peso < 10000:
+    pesolista = list(str(peso))
+    pesolista.insert(3, '.')
+    peso = ''.join(pesolista)
+
+print(altura)
+altura = int(altura)
+if altura < 10:
+  alturalista = list(str(altura))
+  alturalista.insert(0, '0.')
+  altura = ''.join(alturalista)
+  
+elif altura < 100:
+  alturalista = list(str(altura))
+  alturalista.insert(1, '.')
+  altura = ''.join(alturalista)
+
+elif altura < 1000:
+  alturalista = list(str(altura))
+  alturalista.insert(2, '.')
+  altura = ''.join(alturalista)
+
+elif altura < 10000:
+  alturalista = list(str(altura))
+  alturalista.insert(3, '.')
+  altura = ''.join(alturalista)
+
+print(altura)
--- pokemon/pokedex/views.py
+++ pokemon/pokedex/views.py
@@ -6,7 +6,7 @@ import requests
 
 
 def index(request):
-    response = requests.get('https://pokeapi.co/api/v2/pokemon/gloom')
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/snorlax')
     pokemon = response.json()
     nome = pokemon['name'].upper()
     tipo1pokemon = pokemon['types'][0]['type']['name']
@@ -15,10 +15,58 @@ def index(request):
     except:
       tipo2pokemon = 'none'
 
+    peso = pokemon['weight']
+    altura = pokemon['height']
+
+    if peso < 10:
+      pass
+    
+    elif peso < 100:
+        pesolista = list(str(peso))
+        pesolista.insert(1, '.')
+        peso = ''.join(pesolista)
+      
+    elif peso < 1000:
+        pesolista = list(str(peso))
+        pesolista.insert(2, '.')
+        peso = ''.join(pesolista)
+      
+    elif peso < 10000:
+        pesolista = list(str(peso))
+        pesolista.insert(3, '.')
+        peso = ''.join(pesolista)
+    else:
+        peso = 'NaN'
+
+    if altura < 10:
+      alturalista = list(str(altura))
+      alturalista.insert(0, '0.')
+      altura = ''.join(alturalista)
+      
+    elif altura < 100:
+      alturalista = list(str(altura))
+      alturalista.insert(1, '.')
+      altura = ''.join(alturalista)
+  
+    elif altura < 1000:
+      alturalista = list(str(altura))
+      alturalista.insert(2, '.')
+      altura = ''.join(alturalista)
+    
+    elif altura < 10000:
+      alturalista = list(str(altura))
+      alturalista.insert(3, '.')
+      altura = ''.join(alturalista)
+
+    else:
+      altura = 'NaN'
+
     return render(request, 'index.html',{
     'fotopokemon': pokemon['sprites']['other']['official-artwork']['front_default'],
     'nomepokemon': nome,
     'idpokemon': pokemon['id'],
     'tipo1pokemon': tipo1pokemon,
     'tipo2pokemon': tipo2pokemon,
+    'peso': peso,
+    'altura': altura,
     })
